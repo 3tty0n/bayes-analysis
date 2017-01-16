698a39658be4ddb0fca958e19656f30ee0a7438f
Yusuke Izawa <yuizalp@gmail.com>
2017-01-16 20:49:09 +0900

    fix mle.r and add image

--- r/mle.r
+++ r/mle.r
@@ -1,21 +1,40 @@
-setwd("~/workspace/r-analysis")
+data <- read.csv(file("input/data_example1_fit.csv"))
 
-data <- read.csv(file("data/data_example1.csv", encoding = "Shift_JIS"))
-sapporo <- subset(data, prefectures == 1)
-sapporo <- transform(sapporo, kakou_jinkou=sapporo$population/500000)
-sapporo <- transform(sapporo, kakou_kinyu=sapporo$finance/10000)
+likelihood <- function(x) {
+    prefecture <- subset(data, prefectures == x)
 
-H1 = sum(sapporo$kakou_jinkou^2)
-H2 = sum(sapporo$kakou_jinkou)
-H3 = H2
-H4 = length(sapporo$kakou_jinkou)
+    H1 = sum(prefecture$population^2)
+    H2 = sum(prefecture$population)
+    H3 = H2
+    H4 = length(prefecture$population)
 
-v1 = sum(sapporo$kakou_jinkou * sapporo$kakou_kinyu)
-v2 = sum(sapporo$kakou_kinyu)
+    v1 = sum(prefecture$population * prefecture$finance)
+    v2 = sum(prefecture$finance)
 
-H = matrix(c(H1, H2, H3, H4), 2, 2)
-v = matrix(c(v1, v2), 2, 1)
+    H = matrix(c(H1, H2, H3, H4), 2, 2)
+    v = matrix(c(v1, v2), 2, 1)
 
-HInv = solve(H)
+    HInv = solve(H)
 
-ans = crossprod(HInv, v)
+    crossprod(HInv, v)
+}
+
+a <- list()
+b <- list()
+for (x in 1:7) {
+    a[[x]] <- likelihood(x)[1]
+    b[[x]] <- likelihood(x)[2]
+}
+
+png("output/image/mle.png", width=800, height=600)
+par(mfrow=c(2, 4))
+prefecture_list <- c("札幌市", "仙台市", "東京都", "名古屋市", "京都市", "大阪市", "福岡市")
+
+for (i in 1:7) {
+    pre <- subset(data, prefectures == i)
+    plot(pre$population, pre$finance)
+    title(prefecture_list[[i]])
+    abline(b=a[[i]], a=b[[i]], col="blue")
+}
+
+dev.off()
