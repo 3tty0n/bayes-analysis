97325e9740d4afa9ca9cda49e90dff0cba1239cf
Yusuke Izawa <yuizalp@gmail.com>
2017-01-17 19:50:45 +0900

    fix bayes-mcmc-plot.r

--- r/bayes-mcmc-plot.R
+++ r/bayes-mcmc-plot.R
@@ -1,4 +1,4 @@
-data <- read.csv("output/fit-summary-2.txt", sep="\t")
+data <- read.csv("output/fit-summary.txt", sep="\t")
 d <- read.csv("input/data_example1_fit.csv")
 
 a <- list()
@@ -12,7 +12,7 @@ for (j in 10:16) {
   b[[j - 9]] <- data$mean[j]
 }
 
-png('output/image/fit-2.png', width = 800, height = 600)
+png('output/image/fit.png', width = 800, height = 600)
 par(mfrow=c(2, 4))
 prefecture_list <- c("札幌市", "仙台市", "東京都", "名古屋市", "京都市", "大阪市", "福岡市")
 
@@ -20,7 +20,7 @@ for (i in 1:7) {
   pre <- subset(d, prefectures == i)
   plot(pre$population, pre$finance)
   title(prefecture_list[[i]])
-  abline(b=a[[i]], a=b[[i]], col="red")
+  abline(b=a[[i]], a=b[[i]], col="blue")
 }
 
 dev.off()
